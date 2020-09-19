b135dcb5b7cf32f3174268bdb835289f84890018
mh <informermh@gmail.com>
2020-09-19 22:15:28 +0600

    Updates

---
+++ precache-manifest.df272a3c487a9a04fb8bf5ff3452be81.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "156330ea37942561d85bb0d909e59fbb",
+    "url": "/mhtareq93/portfolio/index.html"
+  },
+  {
+    "revision": "b47ad6a53cfb9c0ab72c",
+    "url": "/mhtareq93/portfolio/static/css/main.8b3dcb69.chunk.css"
+  },
+  {
+    "revision": "63e64109b19513c2b83a",
+    "url": "/mhtareq93/portfolio/static/js/2.9e64ffe2.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/mhtareq93/portfolio/static/js/2.9e64ffe2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b47ad6a53cfb9c0ab72c",
+    "url": "/mhtareq93/portfolio/static/js/main.86d18165.chunk.js"
+  },
+  {
+    "revision": "aec83daa65ca46494407",
+    "url": "/mhtareq93/portfolio/static/js/runtime-main.ae46e483.js"
+  },
+  {
+    "revision": "74bf60886887f174e865bbb057232851",
+    "url": "/mhtareq93/portfolio/static/media/1.74bf6088.jpg"
+  }
+]);
